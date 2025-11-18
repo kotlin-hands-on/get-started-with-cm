25a5dfc89ee87e5d7861dece542cfb9c5a863f34
Ilya Evtushenko <evt.ilyaa@gmail.com>
2025-11-18 16:40:06 +0100

    Second migration step: replace androidLibrary with androidMultiplatformLibrary, bump AGP to 9.0, remove kotlinAndroid plugin (use built-in Kotlin)

---
+++ shared/src/commonMain/kotlin/Time.kt
@@ -0,0 +1,15 @@
+package compose.project.demo.composedemo
+
+import kotlinx.datetime.LocalTime
+import kotlinx.datetime.TimeZone
+import kotlinx.datetime.toLocalDateTime
+import kotlin.time.Clock
+
+fun currentTimeAt(location: String, zone: TimeZone): String {
+    fun LocalTime.formatted() = "$hour:$minute:$second"
+
+    val time = Clock.System.now()
+    val localTime = time.toLocalDateTime(zone).time
+
+    return "The time in $location is ${localTime.formatted()}"
+}
