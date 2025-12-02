45b19f8db7b14810f582a060aecb3fc022d7f7fd
Aleksey Zamulla <karikaturkin@gmail.com>
2025-12-03 00:25:28 +0100

    * removed unused code * updated versions of CMP, AGP and Gradle to reasonable ones

--- composeApp/src/commonMain/kotlin/compose/project/demo/composedemo/Greeting.kt
+++
@@ -1,9 +0,0 @@
-package compose.project.demo.composedemo
-
-class Greeting {
-    private val platform = getPlatform()
-
-    fun greet(): String {
-        return "Hello, ${platform.name}!"
-    }
-}
--- composeApp/src/commonMain/kotlin/compose/project/demo/composedemo/Platform.kt
+++
@@ -1,7 +0,0 @@
-package compose.project.demo.composedemo
-
-interface Platform {
-    val name: String
-}
-
-expect fun getPlatform(): Platform
